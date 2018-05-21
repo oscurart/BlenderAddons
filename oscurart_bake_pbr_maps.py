7af70c5fdcd36063d63b9bbc9670432b8a1a83cc
Eugenio Pignataro <info@oscurart.com.ar>
2018-05-21 16:20:03 -0300

    suma opcion de specular

--- oscurart_bake_pbr_maps.py
+++ oscurart_bake_pbr_maps.py
@@ -7,7 +7,7 @@ channels = {"albedo":["AT","DIFFUSE"],
     "normal":["NM","NORMAL"],
     "emit":["EM","EMIT"],
     "roughness":["RO","ROUGHNESS"],
-    "glossy":["GL","GLOSSY"]}
+    "metallic":["ME","GLOSSY"]}
 
 
 #set bake options
@@ -16,6 +16,20 @@ bpy.context.scene.render.bake.use_pass_direct = 0
 bpy.context.scene.render.bake.use_pass_indirect = 0
 bpy.context.scene.render.bake.use_pass_color = 1
 bpy.context.scene.render.bake.use_selected_to_active = 0
+
+
+prps = {}
+
+activeMat = bpy.context.object.active_material
+for node in activeMat.node_tree.nodes:
+    if node.type == "BSDF_PRINCIPLED":
+        if node.inputs['Metallic'].is_linked:            
+            link = activeMat.node_tree.links.new(node.inputs['Specular'],node.inputs['Metallic'].links[0].from_socket)   
+            prps[node] = link         
+        else:
+            prps[node] = node.inputs['Specular'].default_value
+            node.inputs['Specular'].default_value = node.inputs['Metallic'].default_value   
+        
 
 for map in channels.keys():
     imgpath = "%s/IMAGES" % (os.path.dirname(bpy.data.filepath))
@@ -33,3 +47,12 @@ for map in channels.keys():
     img.save()
     bpy.data.images.remove(img)
     activeMat.node_tree.nodes.remove(node)
+    
+    
+for node,value in prps.items():
+    if type(value) == float:
+        node.inputs['Specular'].default_value = value    
+    else:
+        activeMat.node_tree.links.remove(value)
+                
+      
