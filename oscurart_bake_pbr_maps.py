165ba9b36dba7e062c7488353260f8e797eee844
Eugenio Pignataro <info@oscurart.com.ar>
2018-05-21 16:56:20 -0300

    suma opcion de specular

--- oscurart_bake_pbr_maps.py
+++ oscurart_bake_pbr_maps.py
@@ -1,7 +1,7 @@
 import bpy
 import os
 
-size = 2048
+size = 1024
 
 channels = {"albedo":["AT","DIFFUSE"],
     "normal":["NM","NORMAL"],
@@ -20,25 +20,34 @@ bpy.context.scene.render.bake.use_selected_to_active = 0
 
 prps = {}
 
-activeMat = bpy.context.object.active_material
+
+origMat = bpy.context.object.active_material
+activeMat = bpy.context.object.active_material.copy()
+bpy.context.object.active_material = activeMat
+
+
 for node in activeMat.node_tree.nodes:
     if node.type == "BSDF_PRINCIPLED":
         if node.inputs['Metallic'].is_linked:            
-            link = activeMat.node_tree.links.new(node.inputs['Specular'],node.inputs['Metallic'].links[0].from_socket)   
-            prps[node] = link         
+            link = activeMat.node_tree.links.new(node.inputs['Specular'],node.inputs['Metallic'].links[0].from_socket)  
+            node.inputs["Metallic"].default_value = 0 
+            prps[node] = link      
         else:
             prps[node] = node.inputs['Specular'].default_value
             node.inputs['Specular'].default_value = node.inputs['Metallic'].default_value   
-        
+
+
+for link in activeMat.node_tree.links:
+    if link.to_socket.name == "Metallic":
+        activeMat.node_tree.links.remove(link)   
 
 for map in channels.keys():
     imgpath = "%s/IMAGES" % (os.path.dirname(bpy.data.filepath))
-    activeMat = bpy.context.object.active_material
     img = bpy.data.images.new(channels[map][0],  width=size, height=size, alpha=True)
     print (channels[map][0])
     if channels[map][0] != "AT":
         img.colorspace_settings.name = 'Linear' 
-    img.filepath = "%s/%s_%s.png" % (imgpath, activeMat.name, channels[map][0])
+    img.filepath = "%s/%s_%s.png" % (imgpath, activeMat.name.replace(".001",""), channels[map][0])
     node = activeMat.node_tree.nodes.new("ShaderNodeTexImage")
     node.image = img
     activeMat.node_tree.nodes.active = node
@@ -55,4 +64,5 @@ for node,value in prps.items():
     else:
         activeMat.node_tree.links.remove(value)
                 
-      
+bpy.data.materials.remove(activeMat)      
+bpy.context.object.active_material = origMat
