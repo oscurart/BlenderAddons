ab4287b2f2ccd1f4d4777601f1a0a6cbaf396c7e
Oscurart <info@oscurart.com.ar>
2018-10-02 14:17:25 -0300

    add new script

--- oscurart_join_meshes_uv_parts.py
+++ oscurart_join_meshes_uv_parts.py
@@ -23,50 +23,67 @@ import bmesh
 
 #variables
 parts = 2
+partsY = 1 #zero for non up
 
 
 me = bpy.data.meshes.new("MergeObject")
 
 indexSuma = 0
+faceSuma = 0
 uvLayerSuma = 0
 totalVertices = []
 totalFaces = []
 uvLayers = {}
 quadrantex = 0
 quadrantey = 0
+smooth = {}
 
 for ob in bpy.context.selected_objects:
     obMatrix = ob.matrix_world    
+    #vert position
     for vert in ob.data.vertices:
-        totalVertices.append(obMatrix * vert.co)        
+        totalVertices.append(obMatrix * vert.co)   
+    #face data collect         
     for face in ob.data.polygons:
         facetemp = []
         for indice in face.vertices[:]:
             facetemp.append(indice+indexSuma)
-        totalFaces.append(facetemp)    
+        totalFaces.append(facetemp)  
+        smooth[face.index+faceSuma] = face.use_smooth
     faces = [face.vertices[:] for face in ob.data.polygons]
+    #uv data
     for i , uvl in enumerate(ob.data.uv_layers[0].data):
         quad = uvl.uv * (1/parts)
         quad[0] += ((1/parts)*quadrantex)
         quad[1] += ((1/parts)*quadrantey)
         uvLayers[i+uvLayerSuma] = quad
+    #incremental variables    
     indexSuma += len(ob.data.vertices)
+    faceSuma += len(ob.data.polygons)
     uvLayerSuma += len(ob.data.uv_layers[0].data)
     quadrantex += 1
     if quadrantex == parts:
         quadrantex = 0
-        quadrantey += 1
-            
+        quadrantey += partsY
+
+#set mesh           
 me.from_pydata(totalVertices,[],totalFaces)
 uv = me.uv_textures.new(name="UVMap")
 
+#set uvs
 for i,uv in enumerate(me.uv_layers[0].data):
     uv.uv = uvLayers[i]
-    
+ 
+#set smooth 
+for i in smooth:
+    me.polygons[i].use_smooth = smooth[i]
+    print(smooth[i])
+
+#link mesh    
 ob = bpy.data.objects.new("MergeObject", me)
 bpy.context.scene.objects.link(ob)
 
-
+#active object
 bpy.ops.object.select_all(action="DESELECT")
 ob.select = True
 bpy.context.scene.objects.active = ob
